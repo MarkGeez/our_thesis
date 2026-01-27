7bf035fba6380d6ffce89acfc7fb4958035df01e
stephfenfajardo <fajardo.johnstephen@ue.edu.ph>
2026-01-27 18:44:03 +0800

    certificate forms

---
+++ resources/views/certificate/chairmansignature.blade.php
@@ -0,0 +1,10 @@
+<style>.signature{text-align:right;margin-top:40px}
+    .signature-line{border-top:1px solid #222;width:250px;margin-left:auto;padding-top:4px}
+
+    @media print{}</style>
+<div class="signature" style="margin-top: 175px;">
+        <!-- palitan ng chairman
+        <div class="signature-line">Rolando O. del Rosario</div>
+        -->
+        <div style="font-size:12px;margin-top:6px">Punong Barangay</div>
+</div>
